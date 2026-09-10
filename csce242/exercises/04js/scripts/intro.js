5c2bf763ae37fece1241c7e9452de2b033c10a03
Dorian Rhone <drhone@email.sc.edu>
2026-09-10 12:32:26 -0400

    04js exercise

--- csce242/exercises/04js/scripts/intro.js
+++ csce242/exercises/04js/scripts/intro.js
@@ -8,3 +8,9 @@ document.getElementById("link").onclick = (e) => {
     e.preventDefault(); //don't go to links destinsation
     e.target.classList.add("cool-link");
 };
+
+// When the bounce button is clicked make the ball bounce
+document.getElementById("btn-bounce").onclick = (e) => {
+    console.log("bounce button clicked");
+    document.getElementById("ball").classList.toggle("bouncing-ball");
+}
